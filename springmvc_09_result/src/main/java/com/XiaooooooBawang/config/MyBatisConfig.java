7f2e32b21f8b5508e22b474582d6b94a481dc3aa
XiaooooooBawang <1206286599@qq.com>
2022-07-29 23:23:45 +0800

    Update springMVC

---
+++ springmvc_09_result/src/main/java/com/XiaooooooBawang/config/MyBatisConfig.java
@@ -0,0 +1,24 @@
+package com.XiaooooooBawang.config;
+
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+public class MyBatisConfig {
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource) {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setTypeAliasesPackage("com.XiaooooooBawang.model");
+        return sqlSessionFactoryBean;
+    }
+
+    @Bean
+    public MapperScannerConfigurer mapperScannerConfigurer() {
+        MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
+        mapperScannerConfigurer.setBasePackage("com.XiaooooooBawang.dao");
+        return mapperScannerConfigurer;
+    }
+}
