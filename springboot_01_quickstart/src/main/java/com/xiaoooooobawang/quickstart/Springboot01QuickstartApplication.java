236517b1701e1a368a47b3cd897f424641830fc7
XiaooooooBawang <1206286599@qq.com>
2022-08-04 18:17:14 +0800

    update springboot

--- springboot_01_quickstart/src/main/java/com/xiaoooooobawang/quickstart/Springboot01QuickstartApplication.java
+++ springboot_01_quickstart/src/main/java/com/xiaoooooobawang/quickstart/Springboot01QuickstartApplication.java
@@ -1,13 +1,18 @@
 package com.xiaoooooobawang.quickstart;
 
+import com.xiaoooooobawang.quickstart.controller.BookController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Springboot01QuickstartApplication {
-
+    //当前运行的这个类在SpringBoot程序中是所有功能的入口，称为<font color="#ff0000"><b>引导类</b></font>。
     public static void main(String[] args) {
-        SpringApplication.run(Springboot01QuickstartApplication.class, args);
+        //SpringBoot程序启动还是创建了一个Spring容器对象，我们是可以获取bean的
+        ConfigurableApplicationContext context = SpringApplication.run(Springboot01QuickstartApplication.class, args);
+        BookController bookController = context.getBean(BookController.class);
+        System.out.println(bookController);
     }
 
 }
